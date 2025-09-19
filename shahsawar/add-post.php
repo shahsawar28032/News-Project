dd3e5be03b8b7a0b07115713a8c0ec192482052a
Shah Sawar khan <shahsawarkhan28032@gmail.com>
2025-09-19 07:19:32 +0500

    Update add-post.php

--- shahsawar/add-post.php
+++ shahsawar/add-post.php
@@ -1,23 +1,23 @@
 <?php include "header.php"; ?>
   <div id="admin-content">
       <div class="container">
-         <div class="row">
+         <div class="row justify-content-center">
              <div class="col-md-12">
                  <h1 class="admin-heading">Add New Post</h1>
              </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form -->
-                  <form  action="save-post.php" method="POST" enctype="multipart/form-data">
+                 <form  action="save-post.php" method="POST" enctype="multipart/form-data">
                       <div class="form-group">
-                          <label for="post_title">Title</label>
+                          <label for="post_title" class="mt-3">Title</label>
                           <input type="text" name="post_title" class="form-control" autocomplete="off" required>
                       </div>
                       <div class="form-group">
-                          <label for="exampleInputPassword1"> Description</label>
+                          <label for="exampleInputPassword1" class="mt-3"> Description</label>
                           <textarea name="postdesc" class="form-control" rows="5"  required></textarea>
                       </div>
                       <div class="form-group">
-                          <label for="exampleInputPassword1">Category</label>
+                          <label for="exampleInputPassword1" class="mt-3">Category</label>
                           <select name="category" class="form-control">
                               <option disabled selected> Select Category</option>                              
                               <?php
@@ -35,10 +35,10 @@
                           </select>
                       </div>
                       <div class="form-group">
-                          <label for="exampleInputPassword1">Post image</label>
+                          <label for="exampleInputPassword1" class="mt-3">Post image</label>
                           <input type="file" name="fileToUpload" required>
                       </div>
-                      <input type="submit" name="submit" class="btn btn-primary" value="Save" required />
+                      <input type="submit" name="submit" class="btn btn-primary mt-4" value="Save" required />
                   </form>
                   <!--/Form -->
               </div>
