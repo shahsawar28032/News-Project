32c96ffd27cce88dfdd7a32e7f19de093380d7ba
Shah Sawar khan <shahsawarkhan28032@gmail.com>
2025-09-19 07:16:01 +0500

    Update update-category.php

--- shahsawar/update-category.php
+++ shahsawar/update-category.php
@@ -5,7 +5,7 @@ if($_SESSION["user_role"] == '0'){
 ?>
   <div id="admin-content">
     <div class="container">
-      <div class="row">
+      <div class="row justify-content-center">
         <div class="col-md-12">
             <h1 class="adin-heading">Update Category</h1>
         </div>
@@ -21,13 +21,13 @@ if($_SESSION["user_role"] == '0'){
               <!-- Form Start -->
               <form action="<?php $_SERVER['PHP_SELF']; ?>" method ="POST">
                   <div class="form-group">
-                      <input type="hidden" name="cat_id"  class="form-control" value="<?php echo $row['category_id']; ?>">
+                      <input type="hidden" name="cat_id"  class="form-control mt-2" value="<?php echo $row['category_id']; ?>">
                   </div>
                   <div class="form-group">
                       <label>category Name</label>
-                      <input type="text" name="cat_name" class="form-control" value="<?php echo $row['category_name']; ?>"  placeholder="" required>
+                      <input type="text" name="cat_name" class="form-control mt-2" value="<?php echo $row['category_name']; ?>"  placeholder="" required>
                   </div>
-                  <input type="submit" name="submit" class="btn btn-primary" value="Update" />
+                  <input type="submit" name="submit" class="btn btn-primary mt-3" value="Update" />
               </form>
                <!-- Form End-->
               <?php
