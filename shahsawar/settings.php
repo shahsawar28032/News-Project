b2f9480f0e477607550541aedc2fe737f5b4fcfd
Shah Sawar khan <shahsawarkhan28032@gmail.com>
2025-09-19 07:38:32 +0500

    Update settings.php

--- shahsawar/settings.php
+++ shahsawar/settings.php
@@ -1,7 +1,7 @@
 <?php include "header.php"; ?>
   <div id="admin-content">
       <div class="container">
-         <div class="row">
+         <div class="row justify-content-center">
              <div class="col-md-12">
                  <h1 class="admin-heading">Website Settings</h1>
              </div>
@@ -16,22 +16,22 @@
                     while($row = mysqli_fetch_assoc($result)) {
                 ?>
                   <!-- Form -->
-                  <form  action="save-settings.php" method="POST" enctype="multipart/form-data">
+               <form  action="save-settings.php" method="POST" enctype="multipart/form-data">
                       <div class="form-group">
-                          <label for="website_name">Website Name</label>
+                          <label for="website_name" class="mt-3">Website Name</label>
                           <input type="text" name="website_name" value="<?php echo $row['websitename']; ?>" class="form-control" autocomplete="off" required>
                       </div>
                       <div class="form-group">
-                          <label for="logo">Website Logo</label>
+                          <label for="logo" class="mt-3">Website Logo</label>
                           <input type="file" name="logo">
                           <img src="images/<?php echo $row['logo'];?>" height="100px" width="400px">
                           <input type="hidden" name="old_logo" value="<?php echo $row['logo']; ?>" >
                       </div>
                       <div class="form-group">
-                          <label for="footer_desc">Footer Description</label>
+                          <label for="footer_desc" class="mt-3">Footer Description</label>
                           <textarea name="footer_desc" class="form-control" rows="5" required><?php echo $row['footerdesc']; ?></textarea>
                       </div>
-                       <input type="submit" name="submit" class="btn btn-primary" value="Save" required />
+                       <input type="submit" name="submit" class="btn btn-primary mt-4" value="Save" required />
                   </form>
                   <!--/Form -->
                   <?php
